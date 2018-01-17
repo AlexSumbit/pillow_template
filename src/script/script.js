4e8c0743b1656c7901c9bcfc9d27a193d8518253
AlexSumbit <alexsumbit@gmail.com>
2018-01-18 00:30:48 +0200

    initial commit

---
+++ src/script/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".js-slider-top").slick({
+        autoplay: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        arrows: false,
+        zIndex: -1
+    });
+});
